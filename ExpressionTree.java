ec6d349f925cd730dc67d2d20cbdd3b404a8e995
Jason Zheng <zzheng3@stuy.edu>
2019-04-08 14:37:13 -0400

    Done with toStringPostfix.

--- ExpressionTree.java
+++ ExpressionTree.java
@@ -5,12 +5,11 @@ public class ExpressionTree{
     }
     return "(" + getLeft().toString() + " + " + getRight().toString() + ")";
   }
-
-  /*return the expression as a postfix notation string without parenthesis*/
-  /* The sample tree would be: "3 2 10 * +"     */
   public String toStringPostfix(){
-    /*you are to write this method*/
-    return "";
+    if (isValue) {
+      return getValue();
+    }
+    return getLeft().toStringPostfix() + getRight().toStringPostfix() + getOp();
   }
 
   /*return the expression as a prefix notation string without parenthesis*/
