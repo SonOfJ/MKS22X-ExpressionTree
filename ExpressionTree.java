1a19dee0c9ab3e3a8751c21f86bece26ae2863d0
Jason Zheng <zzheng3@stuy.edu>
2019-04-08 22:56:36 -0400

    Done with apply.

--- ExpressionTree.java
+++ ExpressionTree.java
@@ -23,12 +23,20 @@ public class ExpressionTree{
     }
     return apply(getOp(), getLeft().evaluate(), getRight().evaluate());
   }
-
-  /*use the correct operator on both a and b, and return that value*/
   private double apply(char op, double a, double b){
-    /*you are to write this method*/
-    return 0.0;
+    if (op == '+') {
+      return a + b;
     }
+    if (op == '-') {
+      return a - b;
+    }
+    if (op == '*') {
+      return a * b;
+    }
+    if (op == '/') {
+      return a / b;
+    }
+  }
 
 
 
